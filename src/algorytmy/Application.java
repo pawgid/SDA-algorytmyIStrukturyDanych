6ca76b8746faa111228371c158f36c5b8a4b014a
unknown <pgidaszewski@gmail.com>
2017-02-15 21:02:09 +0100

    Added put, added Add with index, added delete, added toString and some private also

--- src/algorytmy/Application.java
+++ src/algorytmy/Application.java
@@ -5,6 +5,28 @@ import algorytmy.structures.MyList;
 
 public class Application {
     public static void main(String[] args) {
+        MyList myList = initMyList();
+        MyList clonedList = myList.clone();
+        System.out.println(myList);
+        System.out.println(clonedList);
+        System.out.println();
+        clonedList.add(5);
+        System.out.println(myList);
+        System.out.println(clonedList);
+        System.out.println();
+        clonedList.delete(3);
+        System.out.println(myList);
+        System.out.println(clonedList);
+        System.out.println();
+        clonedList.put(0, 4);
+        System.out.println(clonedList);
+        clonedList.add(0, 1);
+        System.out.println(clonedList);
+
+
+    }
+
+    private static MyList initMyList() {
         MyList myList = new MyList();
         myList.add(2);
         myList.add(3);
@@ -19,8 +41,6 @@ public class Application {
         myList.add(-1);
         myList.add(-9);
         myList.add(9);
-        MyList clonedList = myList.clone();
-        clonedList.add(5);
-
+        return myList;
     }
 }
--- src/algorytmy/structures/MyList.java
+++ src/algorytmy/structures/MyList.java
@@ -10,13 +10,36 @@ public class MyList {
         this.size = 0;
     }
 
+    public void put(int index, int value) {
+        if (checkIndex(index)) {
+            this.array[index] = value;
+        }
+    }
+
+    public void add(int index, int value) {
+        if (checkIndex(index)) {
+            if (this.size >= this.array.length) {
+                doubleCapacity();
+            }
+            for (int i = this.size; i > index; i--) {
+                this.array[i] = this.array[i - 1];
+            }
+            this.size++;
+            this.array[index] = value;
+        }
+    }
+
     public int get(int index) {
-        if (index >= 0 && index < size) {
+        if (checkIndex(index)) {
             return array[index];
         } else {
             System.out.println("Wrong index");
             return 0;
         }
+    }
+
+    private boolean checkIndex(int index) {
+        return index >= 0 && index < size;
     }
 
     public void add(int value) {
@@ -47,7 +70,36 @@ public class MyList {
         return myList;
     }
 
+    public void swap(int index1, int index2) {
+        int tmp = this.array[index1];
+        this.array[index1] = this.array[index2];
+        this.array[index2] = tmp;
+    }
+
+    public void delete(int index) {
+        if (checkIndex(index)) {
+            for (int i = index; i < size - 1; i++) {
+//                swap(i, i + 1);
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
     public int getSize() {
         return size;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < this.size; i++) {
+            stringBuilder.append((array[i]));
+            if (i != size - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 }
